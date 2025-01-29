a82dc177706d783c6ca012d8ed02f06c804fbed4
NituRana <niturana0211@gmail.com>
2025-01-29 18:24:45 +0530

    costom iterator

--- decorators_generators/generators.py
+++ decorators_generators/generators.py
@@ -17,3 +17,30 @@ iterator = iter(nums)  # Create an iterator
 # Access elements using the iterator
 print(next(iterator))  # Output: 1
 print(next(iterator))  # Output: 2
+
+# Custom Iterator Class
+'''
+You can create your custom iterator by implementing the __iter__ and __next__ methods.
+
+'''
+
+# Custom Iterator Class
+class Counter:
+    def __init__(self, start, end):
+        self.current = start
+        self.end = end
+
+    def __iter__(self):  # Makes the class an iterator
+        return self
+
+    def __next__(self):
+        if self.current > self.end:
+            raise StopIteration  # End of iteration
+        value = self.current
+        self.current += 1
+        return value
+
+# Usage
+counter = Counter(1, 5)
+for num in counter:
+    print(num)  # Output: 1 2 3 4 5
